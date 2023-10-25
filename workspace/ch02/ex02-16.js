f26df13f6ca5f8b7acac02ad4924b15e8eafc7a6
KIMGEUNDU <vlkoklv@naver.com>
2023-10-25 17:57:35 +0900

    10월 25일 7교시

--- workspace/ch02/ex02-16.js
+++ workspace/ch02/ex02-16.js
@@ -4,18 +4,30 @@ console.log(Math.min(100, 20, 60, 50, 70)); // 20
 
 // 지정한 배열의 요소 중 최소값을 반환
 function smallest(nums) {
-  var min = nums[0];
-  for (var i = 0; i < nums.length; i++) {
+  if (!(nums instanceof Array)) {
     // 첫번째 방법
-    if (nums[i] < min) {
-      min = nums[i];
-    }
+    nums = Array.prototype.slice.call(arguments);
     // 두번째 방법
-    // min = Math.min(min, nums[i])
+    // nums = [...arguments]
   }
-  return min;
+
+  // var min = nums[0];
+  // for (var i = 0; i < nums.length; i++) {
+  //   // 첫번째 방법
+  //   if (nums[i] < min) {
+  //     min = nums[i];
+  //   }
+  //   // 두번째 방법
+  //   // min = Math.min(min, nums[i])
+  // }
+  // return min;
+
+  // 마지막 방법1
+  return Math.min.apply(this, nums);
+  // 마지막 방법2
+  // return Math.min(...nums)
 }
 
 console.log(smallest([10, 100])); // 10
 console.log(smallest([200, 100, 50, 60, 80, 30])); //30
-console.log(smallest([100, 20, 60, 50, 70])); // 20
+console.log(smallest(100, 20, 60, 50, 70)); // 20
--- workspace/ch02/ex02-17.js
+++ workspace/ch02/ex02-17.js
@@ -1,1 +1,48 @@
 // ex02-14.js 복사
+var name = 'global';
+function returnName() {
+  return this.name;
+}
+
+// 일반적인 함수로 호출(함수명())
+console.log(returnName());
+
+// 객체의 메소드로 호출(객체.메소드명())
+var kim = new Object();
+kim.name = '이용복';
+kim.age = 24;
+kim.getName = returnName;
+
+var lee = new Object();
+lee.name = '표지훈';
+lee.age = 31;
+lee.getName = returnName;
+
+console.log('--------------------------------------');
+// 객체를 생성해서 반환하는 함수(생성자)
+function Person(name, age) {
+  // var obj = {};
+
+  if (!(this instanceof Person)) {
+    return new Person(name, age);
+  }
+
+  this.name = name;
+  this.age = age;
+  this.getName = function () {
+    return this.name;
+  };
+
+  // return obj;
+}
+
+var kim = new Person('김철수', 30);
+var lee = new Person('이영희', 35);
+var hong = Person('홍길동', 40);
+
+console.log(kim.age, kim.getName());
+console.log(lee.age, lee.getName());
+
+console.log(kim.getName(), kim.getName.call(lee));
+console.log(lee.getName(), lee.getName.apply(window));
+console.log(returnName(), returnName.call(kim));
