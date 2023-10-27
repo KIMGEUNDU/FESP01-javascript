d02936e7d941c877098eef4a9d876fe06ff3769d
KIMGEUNDU <vlkoklv@naver.com>
2023-10-27 11:33:15 +0900

    10월 27일 2교시

--- workspace/ch04/ex04-07.js
+++ workspace/ch04/ex04-07.js
@@ -1,4 +1,31 @@
-var alert = '출입 금지!';
-console.log(alert);
+// var alert = '출입 금지!';
+// console.log(alert);
 
+// alert('작업 완료.');
+
+// 일반 함수이지만 전역 객체를 오염시킨다.
+// function fn() {
+//   var alert = '출입 금지!';
+//   console.log(alert);
+// }
+
+// fn();
+
+// 즉시 실행 함수
+(function () {
+  var alert = '출입 금지!';
+  console.log(alert);
+})();
+
+// 일반 함수
+// function fn2(alert) {
+//   console.log(alert);
+// }
+
+// 즉시 실행 함수
+fn2('출입 금지!')((alert) => {
+  console.log(alert);
+})('출입 금지!');
+
+// window의 전역 객체
 alert('작업 완료.');
